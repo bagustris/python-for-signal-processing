ff35ea65765340093ebf975f6eea7daa3fde5fb2
bagustris@m049 <bagus@ep.its.ac.id>
2021-05-21 17:25:37 +0900

    add slides

---
+++ check_ipynb.py
@@ -0,0 +1,13 @@
+import json
+
+valid_keys = ['cell_type', 'metadata', 'source']
+filename = './Compressive_Sampling.ipynb'
+
+with open(filename) as f:
+    data = json.load(f)
+
+for index, cell in enumerate(data['cells'], 1):
+    if cell['cell_type'] == 'markdown':
+        extra_keys = [key for key in cell.keys() if key not in valid_keys]
+        if extra_keys:    
+            print(f'Cell {index} has the following keys which are invalid for a markdown cell: {extra_keys}')
